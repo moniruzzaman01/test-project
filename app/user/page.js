52f16889f9495ebc18b8228005cbd57e40d0bbda
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-10-08 13:13:27 +0600

    map

--- app/user/page.js
+++ app/user/page.js
@@ -4,7 +4,13 @@ import { getAllUsers } from "../queries/user-queries";
 export default async function User() {
   const users = await getAllUsers();
   console.log("---------users", users);
-  return <div>User</div>;
+  return (
+    <div>
+      {users.map((user, idx) => (
+        <h1 key={idx}>{user?.firstName}</h1>
+      ))}
+    </div>
+  );
 }
 
 // Add this line - THIS IS CRUCIAL
