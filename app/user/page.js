c45fe9a04792ec32dd7ee7d07a62dcd231bb8ba8
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-10-08 12:43:35 +0600

    dklfjs

--- app/user/page.js
+++ app/user/page.js
@@ -1,3 +1,4 @@
+// app/user/page.tsx
 import { getAllUsers } from "../queries/user-queries";
 
 export default async function User() {
@@ -5,3 +6,6 @@ export default async function User() {
   console.log("---------users", users);
   return <div>User</div>;
 }
+
+// Add this line - THIS IS CRUCIAL
+export const dynamic = "force-dynamic";
